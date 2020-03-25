4a5692320c1cdc91209215b1d231922907a8866b
Psilon <pzixel@gmail.com>
2020-03-25 20:48:09 +0300

    remove unsafe, add retry

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,44 @@
 use chrono::Local;
 use dirs::home_dir;
 use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+use retry;
+use std::env::args;
+use std::error::Error;
 use std::ffi::OsString;
-use std::fs;
 use std::num::NonZeroU32;
 use std::path::{Path, PathBuf};
 use std::sync::mpsc::channel;
 use std::time::Duration;
+use std::{fs, io};
 
 #[cfg(windows)]
-fn get_default_path() -> Result<PathBuf, &'static str> {
+fn get_default_path() -> Result<PathBuf, String> {
     match home_dir() {
-        None => Err("Cannot get home directory"),
+        None => Err("Cannot get home directory".into()),
         Some(user_folder_path) => {
             Ok(user_folder_path.join("Documents\\Paradox Interactive\\Europa Universalis IV\\save games"))
         }
     }
 }
 
-fn main() -> Result<(), &'static str> {
-    let path = get_default_path()?;
+fn main() -> Result<(), String> {
+    let args = args().skip(1).next();
+    let path = match args {
+        None => {
+            let path = get_default_path()?;
+            println!("No custom path for save files provided. Using default: {:?}", path);
+            path
+        }
+        Some(path) => {
+            let path: PathBuf = path.into();
+            println!("Custom path for save files provided: {:?}", path);
+            path
+        }
+    };
     let (tx, rx) = channel();
     let mut watcher = watcher(tx, Duration::from_millis(1)).unwrap();
     watcher.watch(path, RecursiveMode::NonRecursive).unwrap();
+    let mut handler = SaveHandler::default();
     loop {
         match rx.recv() {
             Ok(DebouncedEvent::Write(path))
@@ -32,7 +48,9 @@ fn main() -> Result<(), &'static str> {
                         .map(|file| file.to_string_lossy().ends_with("Backup"))
                         .unwrap_or(false) =>
             {
-                handle_save_file(path, NonZeroU32::new(4).unwrap())
+                handler
+                    .handle_save_file(path, NonZeroU32::new(4).unwrap())
+                    .map_err(|e| e.to_string())?
             }
             Ok(ev) => println!("{} Ignore event {:?}", Local::now().to_rfc3339(), ev),
             Err(e) => eprintln!("{} watch error: {:?}", Local::now().to_rfc3339(), e),
@@ -42,20 +60,30 @@ fn main() -> Result<(), &'static str> {
     Ok(())
 }
 
-fn handle_save_file<P: AsRef<Path>>(path: P, frequency: NonZeroU32) {
-    static mut CALLS: u32 = 0;
-        
-    let path = path.as_ref();
-    println!("{} Handling file {:?}", Local::now().to_rfc3339(), path);
-    unsafe {
-        if CALLS % frequency.get() == 0 {
+#[derive(Default)]
+struct SaveHandler {
+    calls: u32,
+}
+
+impl SaveHandler {
+    fn handle_save_file<P: AsRef<Path>>(
+        &mut self,
+        path: P,
+        frequency: NonZeroU32,
+    ) -> Result<(), retry::Error<io::Error>> {
+        let path = path.as_ref();
+        println!("{} Handling file {:?}", Local::now().to_rfc3339(), path);
+        if self.calls % frequency.get() == 0 {
             let new_path = get_new_path(path);
             println!("{} Copying {:?} to {:?}", Local::now().to_rfc3339(), path, new_path);
-            fs::copy(path, new_path);
+            retry::retry(retry::delay::Fixed::from_millis(200).take(10), move || {
+                fs::copy(&path, &new_path)
+            })?;
         } else {
             println!("{} Skipping saving {:?}: Not time yet", Local::now().to_rfc3339(), path);
         }
-        CALLS += 1;
+        self.calls += 1;
+        Ok(())
     }
 }
 
