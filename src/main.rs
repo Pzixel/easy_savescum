42cd4c3190ed8f5441f95d51884d0231a855b9ca
Psilon <pzixel@gmail.com>
2020-03-25 12:07:41 +0300

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
-use std::path::{PathBuf, Path};
+use chrono::Local;
 use dirs::home_dir;
-use notify::{Watcher, RecursiveMode, watcher, DebouncedEvent};
+use notify::{watcher, DebouncedEvent, RecursiveMode, Watcher};
+use std::ffi::OsString;
+use std::fs;
+use std::num::NonZeroU32;
+use std::path::{Path, PathBuf};
 use std::sync::mpsc::channel;
 use std::time::Duration;
-use chrono::Local;
-use std::num::NonZeroU32;
-use std::fs;
-use std::ffi::OsString;
 
 #[cfg(windows)]
 fn get_default_path() -> Result<PathBuf, &'static str> {
     match home_dir() {
         None => Err("Cannot get home directory"),
-        Some(user_folder_path) =>
-            Ok(user_folder_path.join("Documents\\Paradox Interactive\\Europa Universalis IV\\save games")),
+        Some(user_folder_path) => {
+            Ok(user_folder_path.join("Documents\\Paradox Interactive\\Europa Universalis IV\\save games"))
+        }
     }
-
 }
 
 fn main() -> Result<(), &'static str> {
@@ -27,9 +27,14 @@ fn main() -> Result<(), &'static str> {
         match rx.recv() {
             Ok(DebouncedEvent::Write(path))
                 if path.extension().unwrap_or_default() == "eu4"
-                && !path.file_stem().map(|file| file.to_string_lossy().ends_with("Backup")).unwrap_or(false) =>
-                    handle_save_file(path, NonZeroU32::new(4).unwrap()),
-            Ok(_) => {}, // ignore
+                    && !path
+                        .file_stem()
+                        .map(|file| file.to_string_lossy().ends_with("Backup"))
+                        .unwrap_or(false) =>
+            {
+                handle_save_file(path, NonZeroU32::new(4).unwrap())
+            }
+            Ok(_) => {} // ignore
             Err(e) => eprintln!("{} watch error: {:?}", Local::now().format("%Y-%m-%dT%H:%M:%S"), e),
         }
     }
